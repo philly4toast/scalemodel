d500c6b7c8f99d802005687ab0fc4ef841148dfd
philly4toast <phillipyoun@gmail.com>
2020-05-18 20:39:22 -0700

    Forms set up, able to type to change state

--- client/src/AddModelInfo.js
+++ client/src/AddModelInfo.js
@@ -2,11 +2,26 @@ import React from 'react';
 
 
 class AddModelInfo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {      
+      modelName: '',
+      modelDesc: '',
+      picture1: '',
+      picture2: '',
+      completed: false
+    }
+    this.typingModelName = this.typingModelName.bind(this)
+  }
+      typingModelName(event){
+        console.log(event.target.value)
+        this.setState({modelName: event.target.value})
+      }
   render(){
     return (
       <form>
-        <label> Model Name:     
-          <input/>
+        <label> Model Name:
+          <input type="text" name={this.state.modelName} value={this.state.modelName} placeholder='Make and model' onChange={this.typingModelName}/>
         </label>
 
         <label> Description:     
